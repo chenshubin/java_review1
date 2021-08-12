0622ed0725c55dcc07117ffa9b9a3d5d14b7436b
09067436-陈树斌 <09067436@hucais.com>
2021-08-12 21:20:20 +0800

    提交AVA问题

---
+++ juc-learn/src/cn/murphy/ContainerNotSafeDemo.java
@@ -0,0 +1,50 @@
+package cn.murphy;
+
+import sun.rmi.runtime.NewThreadAction;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * java.util.ConcurrentModificationException
+ */
+public class ContainerNotSafeDemo {
+
+    public static void main(String[] args) {
+//        List<String> list = new ArrayList<String>();
+//修改方法1 使用Vector
+//        List<String> list = new Vector<>();
+
+//修改方法2  使用Collations的工具类  Collections.synchronizedList
+//        List<String> list = Collections.synchronizedList(new ArrayList<String>());
+
+//修改方法3 使用 juc里面的类    里面的就是加锁后的数组的copy
+        List<String> list = new CopyOnWriteArrayList<String>();
+
+//java.util.ConcurrentModificationException
+//        for(Integer ingw : list){
+//            if(1==ingw ){
+//                list.add(2);
+//            }
+//        }
+
+        for(int i=1;i<=100;i++){
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0,8));
+                System.out.println(list);
+
+
+            }, String.valueOf(i)).start();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
