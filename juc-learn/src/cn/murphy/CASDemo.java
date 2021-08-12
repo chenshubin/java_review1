0c5db1978297632a4d48890b91a4e60a8278d569
09067436-陈树斌 <09067436@hucais.com>
2021-08-12 17:45:43 +0800

    Initial commit

---
+++ juc-learn/src/cn/murphy/CASDemo.java
@@ -0,0 +1,19 @@
+package cn.murphy;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CASDemo {
+
+
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+
+        System.out.println("修改执行："+atomicInteger.compareAndSet(5,10)  + "，结果："+atomicInteger.get());
+        System.out.println("修改执行："+atomicInteger.compareAndSet(5,11) +"，结果："+atomicInteger.get());
+
+
+        // atomicInteger.getAndIncrement();
+    }
+
+
+}
---
+++ juc-learn/src/cn/murphy/Singletion.java
@@ -0,0 +1,32 @@
+package cn.murphy;
+
+public class Singletion {
+    private  static volatile Singletion singletion = null;
+
+    private Singletion(){
+        System.out.println(Thread.currentThread().getName() +  "这个单例模式");
+    }
+
+
+    public static  Singletion getSingletion(){
+        if(null == singletion){
+
+            synchronized (Singletion.class){
+                if(null == singletion){
+                    singletion = new Singletion();
+                }
+            }
+        }
+        return singletion;
+    }
+
+
+    public static void main(String[] args) {
+
+        for(int i=0;i<10;i++){
+            new Thread(()->{Singletion.getSingletion();},
+                    String.valueOf(i)).start();
+        }
+
+    }
+}
