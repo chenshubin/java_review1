41cd11e946935c7b88e2356a037c0fe4c854b281
09067436-陈树斌 <09067436@hucais.com>
2021-08-12 19:29:45 +0800

    tijiao

---
+++ juc-learn/src/cn/murphy/AtomicReferenceDemo.java
@@ -0,0 +1,43 @@
+package cn.murphy;
+
+import com.oracle.webservices.internal.api.databinding.DatabindingMode;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class AtomicReferenceDemo {
+
+
+
+    public static void main(String[] args) {
+//        AtomicInteger atomicInteger = new AtomicInteger(100);
+
+        AtomicReference<User> atomicReference = new AtomicReference<User>();
+
+        User z3 = new User("z3",22);
+        User li4 = new User("li4",25);
+        atomicReference.set(z3);
+        System.out.println(atomicReference.compareAndSet(z3,li4)+"\t"+atomicReference.get().toString());
+        System.out.println(atomicReference.compareAndSet(z3,li4)+"\t"+atomicReference.get().toString());
+
+    }
+
+}
+
+class User{
+    String username;
+    int age;
+
+    public User(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
