3512b6d83ddb4c19ee2eabb282e005fdfe37644d
09067436-陈树斌 <09067436@hucais.com>
2021-08-16 20:27:44 +0800

    公平锁和非公平锁

---
+++ juc-learn/src/cn/murphy/FreeLock.java
@@ -0,0 +1,22 @@
+package cn.murphy;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ReentrantLock 默认非公平锁
+ */
+public class FreeLock {
+
+    volatile  int n = 0;
+
+    public  void add(){
+        n++;
+    }
+
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+    }
+
+
+}
---
+++ juc-learn/src/cn/murphy/ReenterLockDemo.java
@@ -0,0 +1,83 @@
+package cn.murphy;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 可冲入锁
+ */
+public class ReenterLockDemo {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+//        new Thread(() -> {
+//            try {
+//                phone.sendSms();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }, "t1").start();
+//
+//        new Thread(() -> {
+//            try {
+//                phone.sendSms();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }, "t2").start();
+
+
+        Thread t3 = new Thread(phone,"t3");
+        Thread t4 = new Thread(phone,"t4");
+
+        t3.start();
+        t4.start();
+
+
+    }
+}
+
+
+class Phone implements  Runnable{
+
+
+    public  synchronized  void  sendSms() throws Exception{
+        System.out.println(Thread.currentThread().getName()+"\t invoked sendSMS()");
+        Thread.sleep(1000);
+        this.sendEmail();
+    }
+
+    public  synchronized  void  sendEmail() throws Exception{
+        System.out.println(Thread.currentThread().getName()+"\t invoked sendEmail()");
+    }
+
+
+    Lock lock = new ReentrantLock();
+    public void run(){
+        get();
+    }
+
+
+    public void get() {
+        lock.lock();
+        lock.lock();
+        try{
+            System.out.println(Thread.currentThread().getName()+"\t invoked sendSMS()");
+            set();
+        }finally {
+            lock.unlock();
+            lock.unlock();
+        }
+    }
+
+
+    public void set() {
+        lock.lock();
+        try{
+            System.out.println(Thread.currentThread().getName()+"\t invoked sendEmail()");
+        }finally {
+            lock.unlock();
+        }
+
+    }
+
+}
