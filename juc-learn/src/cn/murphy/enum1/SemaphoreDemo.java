371e112e22c70a82afc8d8eb8e93f6437b70fc02
09067436-陈树斌 <09067436@hucais.com>
2021-08-24 19:19:15 +0800

    信号量

---
+++ juc-learn/src/cn/murphy/enum1/SemaphoreDemo.java
@@ -0,0 +1,38 @@
+package cn.murphy.enum1;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * 信号量主要用于两个目的，一个是用于多个共享资源的互斥使用，另一个用于并发线程的控制
+ */
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(30,false); //模拟只有3个线程容量
+
+        for (int i =1;i<=100;i++){ //模拟6个线程去抢
+            new Thread(() -> {
+
+                try {
+                    semaphore.acquire();
+                    System.out.println(Thread.currentThread().getName()+"\t线程抢占资源");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        Thread.sleep(6000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    semaphore.release();
+                    System.out.println(Thread.currentThread().getName()+"\t线程释放资源");
+                }
+
+
+            }, ""+i).start();
+        }
+
+
+    }
+
+}
