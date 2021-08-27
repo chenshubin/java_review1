6276dd3b4f401ab664795d89dc28ffa637e33276
09067436-陈树斌 <09067436@hucais.com>
2021-08-27 17:45:55 +0800

    线程池

---
+++ juc-learn/src/cn/murphy/threadpool/MyThreadPoolDemo.java
@@ -0,0 +1,7 @@
+package cn.murphy.threadpool;
+
+public class MyThreadPoolDemo {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
