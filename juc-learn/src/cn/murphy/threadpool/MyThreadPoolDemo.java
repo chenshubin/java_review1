ce5dc161b139ceef4088f8fbd331d7561234a530
09067436-陈树斌 <09067436@hucais.com>
2021-08-31 16:39:21 +0800

    自定义线程池

--- juc-learn/src/cn/murphy/threadpool/MyThreadPoolDemo.java
+++ juc-learn/src/cn/murphy/threadpool/MyThreadPoolDemo.java
@@ -5,8 +5,53 @@ import java.util.concurrent.*;
 public class MyThreadPoolDemo {
     public static void main(String[] args) {
         System.out.println(Runtime.getRuntime().availableProcessors());
+//        threadPoolInit();
 
-//        ThreadPoolExecutor
+        ExecutorService threadPool = new ThreadPoolExecutor(
+        2, //开放2个窗口
+        5,//设置最大窗口
+        1L,//可以摸鱼的时间
+        TimeUnit.SECONDS,//时间单位
+        new LinkedBlockingDeque<>(3),//设置等候位置
+        Executors.defaultThreadFactory() ,//默认的生成工厂
+//        new ThreadPoolExecutor.AbortPolicy()//拒绝策略
+//        new ThreadPoolExecutor.CallerRunsPolicy()// 调用者运行
+//                new ThreadPoolExecutor.DiscardOldestPolicy()//丢弃等待最久的
+        new ThreadPoolExecutor.DiscardPolicy()//满了就拒收
+        );
+
+
+
+        try{
+            for(int i = 0 ; i<10; i++){
+                int finalI = i;
+                threadPool.execute(()->{
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+"\t"+ finalI +" 办理业务");
+
+
+                });
+            }
+        }catch ( Exception e){
+            e.printStackTrace();
+        }finally {
+            threadPool.shutdown();
+        }
+
+
+
+
+    }
+
+    /**
+     * 创建jdk自带的线程池
+     */
+    private static void threadPoolInit() {
+        //        ThreadPoolExecutor
 //        ScheduledExecutorService
 
         //Array arrays
@@ -18,17 +63,17 @@ public class MyThreadPoolDemo {
 //        Executors.newSingleThreadExecutor();//一个任务一个线程
 //        Executors.newCachedThreadPool();//使用执行很多
 
-//        ExecutorService threadPool = Executors.newFixedThreadPool(5);//一池5个处理线程
 
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);//一池5个处理线程
 
 //        ExecutorService threadPool = Executors.newSingleThreadExecutor();//一池一个处理线程
 
-        ExecutorService threadPool = Executors.newCachedThreadPool();//一池N个处理线程 //系统自动决定
+//        ExecutorService threadPool = Executors.newCachedThreadPool();//一池N个处理线程 //系统自动决定
 
 
         //模拟100 个用户办理业务，每个用户就是一个来自外部的请求
         try{
-            for(int i = 0 ; i<100; i++){
+            for(int i = 0 ; i<1000; i++){
                 int finalI = i;
                 threadPool.execute(()->{
 
@@ -45,7 +90,5 @@ public class MyThreadPoolDemo {
         }finally {
             threadPool.shutdown();
         }
-
-
     }
 }
