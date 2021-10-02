c3044dbb51c530bc645d9c2c35008ab538d38333
09067436-陈树斌 <09067436@hucais.com>
2021-10-02 09:23:55 +0800

    增加spring代码

--- spring-learn/src/main/java/cn/murphy/springlearn/cycledep/ClientConstructor.java
+++ spring-learn/src/main/java/cn/murphy/springlearn/cycledep/ClientConstructor.java
@@ -1,6 +1,22 @@
 package cn.murphy.springlearn.cycledep;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/bank")
 public class ClientConstructor {
+    @Resource
+    ServiceA serviceA;
+
+    @GetMapping("helloworld")
+    public String helloworld(){
+        System.out.println(serviceA.toString());
+            return "hello";
+    }
 
 
     public static void main(String[] args) {
